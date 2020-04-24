438e25542b69335e1f455a4bce35982258e05393
Annie Zhang <azhang@quartermaster.house>
2020-04-23 22:48:54 -0400

    Add list of todos notes

---
+++ TODO.js
@@ -0,0 +1,7 @@
+// state management (refactor)
+// optimistic loading on adding new scores
+// animations - css? library?
+// one get call or multiple based on type?
+// set up auth0
+// redesign
+// find appropriate UX good practices
