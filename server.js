e87e029207d0ad49cc67f3a3180164a0066da780
Annie Zhang <azhang@quartermaster.house>
2020-05-03 15:27:45 -0400

    Fix path

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ app.use(express.json());
 app.use("/scores", scoreRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('build'));
 }
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 //Connect to DB
